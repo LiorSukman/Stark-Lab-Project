c6ac32f37caf9252bb117c4c207e5decb090e3bc
Lior Sukman <lior.sukman@gmail.com>
2021-12-03 19:28:59 +0200

    make more general

--- ml/plot_tests.py
+++ ml/plot_tests.py
@@ -91,25 +91,28 @@ def plot_cf(tp, tn, fp, fn, tp_std, tn_std, fp_std, fn_std, cz_ax, cz, v_max):
     _ = sns.heatmap(data, annot=labels, fmt='', vmin=0, vmax=1, cmap=cmap, ax=cz_ax, xticklabels=ticklabels,
                     yticklabels=ticklabels)
     cz_ax.set_title(f"Chunk size={cz}")
+    cz_ax.set_ylabel("True Label")
+    cz_ax.set_xlabel('Predicted Label')
 
 
 def plot_conf_mats(df, restriction, name=None, chunk_size=[0], modalities=None):
-    data_path = f'../data_sets_new/{restriction}_0/spatial/0_0.800.2/'
+    df_temp = df.copy()
+    data_path = f'../data_sets/{restriction}_0/spatial/0_0.800.2/'
     _, _, test, _, _, _ = ML_util.get_dataset(data_path)
     test = np.squeeze(test)
     positive, negative = np.sum(test[:, -1] == 1), np.sum(test[:, -1] == 0)
     if modalities is None:
         modalities = [('spatial', SPATIAL), ('temporal', TEMPORAL), ('morphological', MORPHOLOGICAL)]
 
-    tpp = df.pyr_acc * 0.01
-    tnp = df.in_acc * 0.01
+    tpp = df_temp.pyr_acc * 0.01
+    tnp = df_temp.in_acc * 0.01
 
-    df['tp'] = positive * tpp
-    df['tn'] = negative * tnp
-    df['fp'] = negative * (1 - tnp)
-    df['fn'] = positive * (1 - tpp)
+    df_temp['tp'] = positive * tpp
+    df_temp['tn'] = negative * tnp
+    df_temp['fp'] = negative * (1 - tnp)
+    df_temp['fn'] = positive * (1 - tpp)
 
-    grouped = complete.groupby(by=['restriction', 'modality', 'chunk_size'])
+    grouped = df_temp.groupby(by=['restriction', 'modality', 'chunk_size'])
     mean = grouped.mean()
     std = grouped.std()
 
@@ -135,7 +138,7 @@ def get_title(restriction):
     seeds = np.arange(20)
     tot, tst, pyr, intn = [], [], [], []
     for seed in seeds:
-        data_path = f"../data_sets_new/{restriction}_{seed}/spatial/0_0.800.2/"
+        data_path = f"../data_sets/{restriction}_{seed}/spatial/0_0.800.2/"
         train, dev, test, _, _, _ = ML_util.get_dataset(data_path)
         tot.append(len(train) + len(dev) + len(test))
         tst.append(len(test))
@@ -172,10 +175,13 @@ def get_labels(lst):
 
 
 def plot_fet_imp(df, sems, restriction, name=None, chunk_size=None, modalities=None):
-    # title = get_title(restriction)
+    title = get_title(restriction)
     if modalities is None:
-        modalities = [('spatial', SPATIAL), ('temporal', TEMPORAL), ('spat_tempo', SPAT_TEMPO),
-                      ('morphological', MORPHOLOGICAL)]
+        modalities = [('spatial', SPATIAL), ('temporal', TEMPORAL), ('morphological', MORPHOLOGICAL)]
+
+    if chunk_size is None:
+        chunk_size = get_labels([ind[2] for ind in df.index])
+
     fets_org = [f"feature {f + 1}" for f in range(NUM_FETS)]
     map = {f: name for (f, name) in zip(fets_org, fet_names)}
     df = df.rename(map, axis='columns')
@@ -183,42 +189,27 @@ def plot_fet_imp(df, sems, restriction, name=None, chunk_size=None, modalities=N
     sems = sems.rename(map, axis='columns')
     sems = sems.drop(columns=['seed', 'acc', 'auc', 'pyr_acc', 'in_acc'])
     for m_name, m_places in modalities:
-        df_m = np.asarray(df.loc[:, m_name, :].dropna(axis=1))
-        sems_m = np.asarray(sems.loc[:, m_name, :].dropna(axis=1))
+        df_m = df.xs(m_name, level="modality").dropna(axis=1)
+        sems_m = sems.xs(m_name, level="modality").dropna(axis=1)
         names_m = np.asarray(fet_names)[m_places[:-1]]
 
-        order = np.argsort((-1 * df_m).mean(axis=0))
-        df_m = df_m[:, order]
-        sems_m = sems_m[:, order]
+        df_order = np.asarray(df_m)
+        order = np.argsort((-1 * df_order).mean(axis=0))
+
         names_m = names_m[order]
 
         x = np.arange(len(names_m))  # the label locations
 
-        if chunk_size is None:
-            v0, e0 = df_m[0], sems_m[0]
-            v200, e200 = df_m[1], sems_m[1]
-            v500, e500 = df_m[2], sems_m[2]
-        else:
-            d = {0: 0, 200: 1, 500: 2}
-            v, e = df_m[d[chunk_size]], sems_m[d[chunk_size]]
-            order = np.argsort((-1 * v))
-            v = v[order]
-            e = e[order]
-            names_m = names_m[order]
-
-        width = 0.3  # the width of the bars
+        width = 0.5 / (len(modalities) + 1)  # the width of the bars
 
         fig, ax = plt.subplots(figsize=(6, 12))
-        if chunk_size is None:
-            _ = ax.bar(x - width, v0, width - 0.02, label='chunk_size = 0', yerr=e0, color='#404040',
-                       edgecolor='k')
-            _ = ax.bar(x, v500, width - 0.02, label='chunk_size = 500', yerr=e500, color='#808080',
-                       edgecolor='k')
-            _ = ax.bar(x + width, v200, width - 0.02, label='chunk_size = 200', yerr=e200,
-                       color='#ffffff', edgecolor='k')
-        else:
-            _ = ax.bar(x, v, width - 0.02, yerr=e, color='#404040', edgecolor='k')
 
+        for i, cz in enumerate(chunk_size):
+            df_cz = np.asarray(df_m.xs(cz, level="chunk_size"))[:, order].flatten()
+            sems_cz = np.asarray(sems_m.xs(cz, level="chunk_size"))[:, order].flatten()
+
+            ax.bar(x - (i - len(chunk_size) // 2) * width, df_cz, width, label=f'chunk_size = {cz}', yerr=sems_cz,
+                   edgecolor='k')
         # Add some text for labels, title and custom x-axis tick labels, etc.
         ax.set_ylabel('Importance')
         # ax.set_title(title)
@@ -229,6 +220,7 @@ def plot_fet_imp(df, sems, restriction, name=None, chunk_size=None, modalities=N
         fig.tight_layout()
 
         if name is None:
+            plt.suptitle(title)
             plt.show()
         else:
             plt.savefig(SAVE_PATH + f"{name}_fet_imp.pdf", transparent=True)
@@ -275,51 +267,28 @@ def plot_acc_vs_auc(df, name=None, chunk_size=[0], modalities=None):
         else:
             plt.savefig(SAVE_PATH + f"{name}_{m_name}_acc_vs_auc.pdf", transparent=True)
 
+
 def plot_results(df, sems, restriction, acc=True, name=None, chunk_size=None):
     if name is None:
         title = get_title(restriction)
 
     labels = get_labels([ind[1] for ind in df.index])
 
-    if acc:
-        if chunk_size is not None:
-            val = df.xs(chunk_size, level="chunk_size").acc
-            sem = sems.xs(chunk_size, level="chunk_size").acc
-        else:
-            zero = df.xs(0, level="chunk_size").acc
-            five_hundred = df.xs(500, level="chunk_size").acc
-            two_hundred = df.xs(200, level="chunk_size").acc
-            zero_sem = sems.xs(0, level="chunk_size").acc
-            five_hundred_sem = sems.xs(500, level="chunk_size").acc
-            two_hundred_sem = sems.xs(200, level="chunk_size").acc
-    else:
-        if chunk_size is not None:
-            val = df.xs(chunk_size, level="chunk_size").auc
-            sem = sems.xs(chunk_size, level="chunk_size").auc
-        else:
-            zero = df.xs(0, level="chunk_size").auc
-            five_hundred = df.xs(500, level="chunk_size").auc
-            two_hundred = df.xs(200, level="chunk_size").auc
-            zero_sem = sems.xs(0, level="chunk_size").auc
-            five_hundred_sem = sems.xs(500, level="chunk_size").auc
-            two_hundred_sem = sems.xs(200, level="chunk_size").auc
+    if chunk_size is None:
+        chunk_size = get_labels([ind[2] for ind in df.index])
+
+    fig, ax = plt.subplots()
 
     x = np.arange(len(labels))  # the label locations
-    width = 0.3  # the width of the bars
+    width = 1 / (len(chunk_size) + 1)  # the width of the bars
 
-    fig, ax = plt.subplots(figsize=(6, 12))
-    if chunk_size is None:
-        rects1 = ax.bar(x - width, zero, width - 0.02, label='chunk_size = 0', yerr=zero_sem, color='#404040',
-                        edgecolor='k')
-        rects2 = ax.bar(x, five_hundred, width - 0.02, label='chunk_size = 500', yerr=five_hundred_sem, color='#808080',
-                        edgecolor='k')
-        rects3 = ax.bar(x + width, two_hundred, width - 0.02, label='chunk_size = 200', yerr=two_hundred_sem,
-                        color='#ffffff', edgecolor='k')
-        autolabel(rects1, ax, acc)
-        autolabel(rects2, ax, acc)
-        autolabel(rects3, ax, acc)
-    else:
-        rects = ax.bar(x, val, width - 0.02, yerr=sem, color='#404040', edgecolor='k')
+    for i, cz in enumerate(chunk_size):
+        col = 'acc' if acc else 'auc'
+        val = df.xs(cz, level="chunk_size")[col]
+        sem = sems.xs(cz, level="chunk_size")[col]
+
+        rects = ax.bar(x - (i - len(chunk_size) // 2) * width, val, width, label=f'chunk_size = {cz}', yerr=sem,
+                       edgecolor='k')
         autolabel(rects, ax, acc)
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
@@ -332,6 +301,7 @@ def plot_results(df, sems, restriction, acc=True, name=None, chunk_size=None):
     fig.tight_layout()
 
     if name is None:
+        plt.suptitle(title)
         plt.show()
     else:
         plt.savefig(SAVE_PATH + f"{name}_{'acc' if acc else 'auc'}.pdf", transparent=True)
@@ -345,20 +315,17 @@ if __name__ == "__main__":
     3) Have we the correct path for the confusion matrix?
     """
     model = 'rf'
-    results = pd.read_csv(f'results_{model}_f1.csv', index_col=0)
+    results = pd.read_csv(f'results_{model}_shap.csv', index_col=0)
     complete = results[results.restriction == 'complete']
-    # complete = complete[complete.modality == 'spatial']
     no_small_sample = results[results.restriction == 'no_small_sample']
     grouped_complete = complete.groupby(by=['restriction', 'modality', 'chunk_size'])
     grouped_no_small_sample = no_small_sample.groupby(by=['restriction', 'modality', 'chunk_size'])
     plot_acc_vs_auc(complete)
-    # plot_conf_mats(complete, 'complete')
+    plot_conf_mats(complete, 'complete',
+                   modalities=[('spatial', SPATIAL), ('temporal', TEMPORAL), ('morphological', MORPHOLOGICAL)],
+                   chunk_size=[0, 1600, 800, 400, 200, 100])
     # plot_roc_curve(complete)
-    exit(0)
 
-    plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=True)
-    plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=False)
+    plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=True, chunk_size=None)
+    plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=False, chunk_size=None)
     plot_fet_imp(grouped_complete.mean(), grouped_complete.sem(), 'complete')
-    exit(0)
-    plot_results(grouped_no_small_sample.mean(), grouped_no_small_sample.sem(), 'no_small_sample', acc=True)
-    plot_results(grouped_no_small_sample.mean(), grouped_no_small_sample.sem(), 'no_small_sample', acc=False)
