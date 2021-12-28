new statistics, fix auc problem, partly fix fints, psth, trans units

clusters.py
@@ -160,7 +160,7 @@ class Cluster(object):
             mean_spike = self.calc_mean_waveform()
             mean_spike.plot_spike(ax)
         else:
-            fig, ax = plt.subplots(NUM_CHANNELS, 1, sharex=True, sharey=True, figsize=(6, 20))
+            fig, ax = plt.subplots(NUM_CHANNELS, 1, sharex=True, sharey=True, figsize=(3, 20))
             mean_spike = self.np_spikes.mean(axis=0)
             std_spike = self.np_spikes.std(axis=0)
             color1 = PYR_COLOR if self.label == 1 else PV_COLOR if self.label == 0 else UT_COLOR
@@ -171,7 +171,8 @@ class Cluster(object):
                 c_ax.plot(np.arange(TIMESTEPS), mean_channel, c=color1)
                 c_ax.fill_between(np.arange(TIMESTEPS), mean_channel - std_channel, mean_channel + std_channel,
                                   color=color2, alpha=0.2)
-                c_ax.axis('off')
+                if i != 0:
+                    c_ax.axis('off')
             if not save:
                 fig.suptitle(f"Cluster {self.get_unique_name()} of type {'PYR' if self.label == 1 else 'IN' if self.label == 0 else 'UT' }")
                 plt.show()

ml/plot_tests.py
@@ -18,16 +18,58 @@ NUM_FETS = 30
 SAVE_PATH = '../../../data for figures/New/'
 
 
-def change_length(y, x, length):
-    valid = [0] + list(np.argwhere(np.convolve(x, [1, -1], 'same') != 0).flatten())
+def change_length(x, y, length):
+    valid = []
+    for i in range(len(x)):
+        if i == len(x) - 1:
+            valid.append(i)
+        elif x[i] != x[i + 1]:
+            valid.append(i)
+        else:
+            continue
+
     x = x[valid]
     y = y[valid]
-    y[-1] = 1
 
     xnew = np.linspace(0, 1, length)
     f = interpolate.interp1d(x, y, kind='linear')
-    ynew = f(xnew)
-    return ynew
+    ynew = [0] + list(f(xnew))
+    return np.array(ynew)
+
+
+def my_change_length(x, y, length):
+    xnew = np.linspace(0, 1, length)
+    ynew = []
+    loc = 0
+
+    assert x[0] == 0 and x[-1] == 1 and y[0] == 0 and y[-1] == 1
+
+    for xn in xnew:
+        while True:
+            if loc == len(x) - 1 or loc == 0:
+                ynew.append(y[loc])
+                if loc == 0:
+                    loc += 1
+                break
+            elif x[loc - 1] <= xn < x[loc]:
+                if y[loc - 1] == y[loc]:
+                    ynew.append(y[loc - 1])
+                else:
+                    m = (y[loc - 1] - y[loc]) / (x[loc - 1] - x[loc])
+                    ynew.append(y[loc - 1] + m * (xn - x[loc - 1]))
+                break
+            elif xn > x[loc]:
+                loc += 1
+            elif xn == x[loc]:
+                ynew.append(y[loc])
+                loc += 1
+                break
+            else:
+                raise AssertionError
+
+    ynew[-1] = 1
+
+    return np.array(ynew)
 
 
 def str2lst(str):
@@ -59,9 +101,9 @@ def plot_roc_curve(df, name=None, chunk_size=[200], modalities=None, use_dev=Fal
 
             x_length = 100
 
-            tprs = np.array([change_length(tpr, fpr, x_length) for tpr, fpr in zip(tprs, fprs)])
+            tprs = np.array([change_length(fpr, tpr, x_length) for tpr, fpr in zip(tprs, fprs)])
 
-            mean_fprs = np.linspace(0, 1, x_length)
+            mean_fprs = [0] + list(np.linspace(0, 1, x_length))
             mean_tprs = tprs.mean(axis=0)
             sem = calc_sem(tprs, axis=0)
 
@@ -69,7 +111,9 @@ def plot_roc_curve(df, name=None, chunk_size=[200], modalities=None, use_dev=Fal
 
             auc = df_cz[auc_col].mean()
 
-            ax.plot(mean_fprs, mean_tprs, label=f"AUC={auc:2g}, chunk size={cz}")
+            label_add = f", chunk size = {cz}" if len(chunk_size) > 1 else ""
+
+            ax.plot(mean_fprs, mean_tprs, label=f"AUC={auc:.3f}"+label_add)
             ax.fill_between(mean_fprs, mean_tprs - sem, mean_tprs + sem, alpha=0.2)
 
         ax.plot(mean_fprs, mean_fprs, color='k', linestyle='--', label='chance level')
@@ -88,13 +132,12 @@ def plot_cf(tp, tn, fp, fn, tp_std, tn_std, fp_std, fn_std, cz_ax, cz):
     data = np.array([[tp, fn], [fp, tn]])
     data = data / data.sum(axis=1)
     pn_sign = u"\u00B1"
-    labels = [[f"{tp:2g}{pn_sign}{tp_std:3f}", f"{fn:2g}{pn_sign}{fn_std:3f}"],
-              [f"{fp:2g}{pn_sign}{fp_std:3f}", f"{tn:2g}{pn_sign}{tn_std:3f}"]]
+    labels = [[f"{tp:2g}{pn_sign}{tp_std:.2f}", f"{fn:2g}{pn_sign}{fn_std:.2f}"],
+              [f"{fp:2g}{pn_sign}{fp_std:.2f}", f"{tn:2g}{pn_sign}{tn_std:.2f}"]]
     cmap = sns.light_palette("seagreen", as_cmap=True)
     ticklabels = ['PYR', 'IN']
     _ = sns.heatmap(data, annot=labels, fmt='', vmin=0, vmax=1, cmap=cmap, ax=cz_ax, xticklabels=ticklabels,
                     yticklabels=ticklabels)
-    cz_ax.set_title(f"Chunk size={cz}")
     cz_ax.set_ylabel("True Label")
     cz_ax.set_xlabel('Predicted Label')
 
@@ -138,6 +181,8 @@ def plot_conf_mats(df, restriction, name=None, chunk_size=[0], modalities=None, 
             tp, tn, fp, fn = mean_cz.tp[0], mean_cz.tn[0], mean_cz.fp[0], mean_cz.fn[0]
             tp_std, tn_std, fp_std, fn_std = std_cz.tp[0], std_cz.tn[0], std_cz.fp[0], std_cz.fn[0]
             plot_cf(tp, tn, fp, fn, tp_std, tn_std, fp_std, fn_std, cz_ax, cz)
+            if len(chunk_size) > 1:
+                cz_ax.set_title(f"Chunk size={cz}")
         if name is None:
             plt.show()
         else:
@@ -414,7 +459,7 @@ def plot_results(df, sems, restriction, acc=True, name=None, chunk_size=None, mo
         chunk_size = get_labels([ind[2] for ind in df.index])
 
     if mode == 'bar':
-        fig, ax = plt.subplots(figsize=(6, 9))
+        fig, ax = plt.subplots(figsize=(3 * len(chunk_size) * len(labels), 9))
     else:
         fig, ax = plt.subplots(figsize=(9, 6))
 
@@ -432,14 +477,20 @@ def plot_results(df, sems, restriction, acc=True, name=None, chunk_size=None, mo
             val = df.xs(cz, level="chunk_size")[col]
             sem = sems.xs(cz, level="chunk_size")[col]
 
-            rects = ax.bar(x - (i - len(chunk_size) // 2) * width, val, width, label=f'chunk size={cz}', yerr=sem,
+            rects = ax.bar(x - (i - len(chunk_size) / 2) * width, val, width, label=f'chunk size={cz}', yerr=sem,
                            edgecolor='k', color=colors[i])
             autolabel(rects, ax, acc)
-        ax.set_xticks(x)
-        ax.set_xticklabels(labels)
+
+        chance_level = 0.5 if not acc else 79.8  # TODO make this generic
+        ax.axhline(y=chance_level, xmin=x[0], xmax=x[-1]+2*width*len(chunk_size), color='k', linestyle='--')
+
+        if len(labels) > 1:
+            ax.set_xticks(x)
+            ax.set_xticklabels(labels)
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
         ax.set_ylim(0, 110 if acc else 1.1)
+
     elif mode == 'plot':
         x = np.roll(np.array(chunk_size), -1)
         x[-1] = x[-2] * 2
@@ -494,15 +545,19 @@ if __name__ == "__main__":
     grouped_complete = complete.groupby(by=['restriction', 'modality', 'chunk_size'])
     grouped_no_small_sample = no_small_sample.groupby(by=['restriction', 'modality', 'chunk_size'])
     # plot_acc_vs_auc_bar(complete)
-    # plot_conf_mats(complete, 'complete',
-    #               modalities=[('spatial', SPATIAL), ('temporal', TEMPORAL), ('morphological', MORPHOLOGICAL)],
-    #               chunk_size=[0, 1600, 800, 400, 200, 100])
-    # plot_roc_curve(complete)
+    #plot_conf_mats(complete, 'complete')
+    #plot_roc_curve(complete)
 
-    plot_acc_vs_auc(grouped_complete.mean(), grouped_complete.sem(), 'complete')
+    #plot_acc_vs_auc(grouped_complete.mean(), grouped_complete.sem(), 'complete')
 
-    #plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=True, mode='plot', dev=False)
-    #plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=False, mode='plot', dev=False)
+    #plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=True, mode='bar', dev=False)
+    #plot_results(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=False, mode='bar', dev=False)
     #plot_fet_imp(grouped_complete.mean(), grouped_complete.sem(), 'complete')
     #plot_test_vs_dev(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=True, mode='bar')
     #plot_test_vs_dev(grouped_complete.mean(), grouped_complete.sem(), 'complete', acc=False, mode='bar')
+
+    _, dev, test, _, _, _ = ML_util.get_dataset('../data_sets_region/complete_1/spatial/0_0.800.2/')
+    ca1_labels = dev[:, :,  -1].flatten()
+    ncx_labels = test[:, :,  -1].flatten()
+    print(ncx_labels.mean())
+    print(ca1_labels.mean())

preprocessing_pipeline.py
@@ -38,6 +38,7 @@ punits = {'es04feb12_1_3_2',
           'm649r1_22_2_3',
           'm649r1_22_3_2'}
 
+
 def load_cluster(load_path, name):
     files = [load_path + f for f in listdir(load_path) if isfile(join(load_path, f)) and name + '_' in f]
     assert len(files) == 2
@@ -167,6 +168,7 @@ def create_chunks(cluster, spikes_in_waveform=(200,)):
             ret_spikes.append(res)
 
     return ret_spikes, ret_inds
+
 
 def light_processing(path):
     stats = pd.DataFrame(
@@ -317,7 +319,7 @@ def run(path, chunk_sizes, csv_folder, mat_file, load_path, xml=None, calc_morph
                     spatial_features_mat = calc_spatial_features(rel_data)
                     morphological_features_mat = calc_morphological_features(rel_data)
                     feature_mat_for_cluster = np.concatenate((spatial_features_mat, morphological_features_mat,
-                                                            temporal_features_mat), axis=1)
+                                                              temporal_features_mat), axis=1)
                 else:
                     feature_mat_for_cluster = None
                     for inst in DELTA_MODE:
@@ -325,7 +327,8 @@ def run(path, chunk_sizes, csv_folder, mat_file, load_path, xml=None, calc_morph
                         if feature_mat_for_cluster is None:
                             feature_mat_for_cluster = temp_features_mat
                         else:
-                            feature_mat_for_cluster = np.concatenate((feature_mat_for_cluster, temp_features_mat), axis=1)
+                            feature_mat_for_cluster = np.concatenate((feature_mat_for_cluster, temp_features_mat),
+                                                                     axis=1)
 
                 # Append metadata for the cluster
                 max_abss = np.ones((len(rel_data), 1)) * max_abs
@@ -355,7 +358,7 @@ if __name__ == "__main__":
     parser.add_argument('--dirs_file', type=str, help='path to data directories file', default='dirs.txt')
     parser.add_argument('--chunk_sizes', type=int, help='chunk sizes to create data for, can be a list',
                         default=[0, 100, 200, 400, 800, 1600])
-    parser.add_argument('--save_path', type=str, default='clustersData_no_light_new/',
+    parser.add_argument('--save_path', type=str, default='clustersData_no_light_FINAL/',
                         help='path to save csv files to, make sure the directory exists')
     parser.add_argument('--load_path', type=str, default=TEMP_PATH,
                         help='path to load clusters from, make sure directory exists')

utils/VIS_figures.py
@@ -28,11 +28,23 @@ NUM_CHANNELS = 8
 TIMESTEPS = 32
 UPSAMPLE = 8
 
+# the following values are required to transform the spikes from arbitrary units to V
+# this values are based on the corresponding XML file, change them accordingly if changing units
+VOL_RANGE = 8
+AMPLIFICATION = 1000
+NBITS = 16
 
 def clear():
     plt.clf()
     plt.cla()
     plt.close('all')
+
+
+def trans_units(cluster):
+    data = cluster.spikes
+    data = (data * VOL_RANGE * 1e6) / (AMPLIFICATION * (2 ** NBITS))  # 1e6 for micro V
+    cluster.spikes = data
+    cluster.np_spikes = data
 
 
 def get_main(chunks):
@@ -279,18 +291,17 @@ def fzc_time_lag(clu, name):
 
 
 def ach(bin_range, name, clu):
-    try:
-        hist = np.load(f'./ach_{name}_{bin_range}.npy')
-        return hist
-    except FileNotFoundError:
-        pass
-
-    c = PV_COLOR if name == 'pv' else PYR_COLOR
     N = 2 * bin_range + 2
     offset = 1 / 2
     bins = np.linspace(-bin_range - offset, bin_range + offset, N)
-    chunks = np.array([np.arange(len(clu.timings))])
-    hist = calc_temporal_histogram(clu.timings, bins, chunks)[0]
+    c = PV_COLOR if name == 'pv' else PYR_COLOR
+
+    try:
+        hist = np.load(f'./ach_{name}_{bin_range}.npy')
+    except FileNotFoundError:
+        chunks = np.array([np.arange(len(clu.timings))])
+        hist = calc_temporal_histogram(clu.timings, bins, chunks)[0]
+
     hist_up = signal.resample_poly(hist, UPSAMPLE ** 2, UPSAMPLE, padtype='line')
     hist_up = np.where(hist_up >= 0, hist_up, 0)
 
@@ -298,6 +309,7 @@ def ach(bin_range, name, clu):
 
     fig, ax = plt.subplots()
     ax.bar(bin_inds, hist_up, color=c)
+    ax.set_ylim(0, 60)
     plt.savefig(SAVE_PATH + f"{name}_ACH_{bin_range}.pdf", transparent=True)
     clear()
 
@@ -477,7 +489,7 @@ def compare_densities(df, feature_names):
         clear()
 
 
-def get_comp_results(chunk_size=[0, 100], res_name='results_rf_region'):
+def get_comp_results(chunk_size=[0], res_name='results_rf_region'):
     results = pd.read_csv(f'../ml/{res_name}.csv', index_col=0)
     complete = results[results.restriction == 'complete']
 
@@ -501,17 +513,17 @@ def get_comp_results(chunk_size=[0, 100], res_name='results_rf_region'):
     clear()
 
     # confusion matrices
-    plot_conf_mats(complete, 'complete', name='NCX', chunk_size=[0, 100],
+    plot_conf_mats(complete, 'complete', name='NCX', chunk_size=chunk_size,
                    data_path='../data_sets_region/complete_0/spatial/0_0.800.2/', use_dev=False)
     clear()
-    plot_conf_mats(complete, 'complete', name='CA1', chunk_size=[0, 100],
+    plot_conf_mats(complete, 'complete', name='CA1', chunk_size=chunk_size,
                    data_path='../data_sets_region/complete_0/spatial/0_0.800.2/', use_dev=True)
     clear()
 
     # ROC curves
-    plot_roc_curve(complete, name='NCX', chunk_size=[0, 100], use_dev=False)
+    plot_roc_curve(complete, name='NCX', chunk_size=chunk_size, use_dev=False)
     clear()
-    plot_roc_curve(complete, name='CA1', chunk_size=[0, 100], use_dev=True)
+    plot_roc_curve(complete, name='CA1', chunk_size=chunk_size, use_dev=True)
     clear()
 
 
@@ -524,6 +536,9 @@ if __name__ == '__main__':
     pyr_cluster = load_cluster(TEMP_PATH, pyr_name)
     pv_cluster = load_cluster(TEMP_PATH, pv_name)
 
+    trans_units(pyr_cluster)
+    trans_units(pv_cluster)
+
     # Waveforms
     pyr_cluster.plot_cluster(save=True, path=SAVE_PATH)
     clear()
@@ -532,7 +547,7 @@ if __name__ == '__main__':
 
     # Morphological features - figure 2
 
-    """clu = pyr_cluster
+    clu = pyr_cluster
     color = PYR_COLOR
 
     t2p_fwhm(pyr_cluster, PYR_COLOR, LIGHT_PYR, 'pyr')
@@ -564,7 +579,7 @@ if __name__ == '__main__':
     pyr_train = pyr_cluster.timings[pyr_mask]
     pv_train = pv_cluster.timings[pv_mask]
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 1))
     ax.axis('off')
     ax.vlines(pyr_train, 0.05, 0.45, colors=PYR_COLOR)
     ax.vlines(pv_train, 0.55, 0.95, colors=PV_COLOR)
@@ -609,7 +624,7 @@ if __name__ == '__main__':
     plot_delta(pyr_cluster, 'pyr')
     plot_delta(pv_cluster, 'pv')
 
-    get_delta_results('morphological')"""
+    get_delta_results('morphological')
 
     # Spatial features - figure 5
 
@@ -633,16 +648,16 @@ if __name__ == '__main__':
 
     # chunks - fig 6
     
-    """chunk_fig(pyr_cluster, 'pyr', 200)
+    chunk_fig(pyr_cluster, 'pyr', 200)
 
     chunk_results()
 
     # nCX vs CA1 - fig 7
     
-    feature_names = ['fwhm', 'trough2peak', 'unif_dist', 'd_kl_mid', 'spatial_dispersion_sd', ' fzc_red']
+    feature_names = ['fwhm', 'trough2peak', 'unif_dist', 'd_kl_mid', 'spatial_dispersion_sd', 'fzc_red']
     features_df = feature_names + ['region', 'label']
     df = load_df(features_df, trans_labels=False)
 
     compare_densities(df, feature_names)
 
-    get_comp_results()"""
+    get_comp_results()

utils/calc_psth.py
@@ -2,11 +2,18 @@ from light_removal import combine_list
 from preprocessing_pipeline import load_cluster
 import glob
 import scipy.io as io
+import scipy.stats as stats
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from enum import Enum
-from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+from constants import PV_COLOR, LIGHT_PV
+
+import matplotlib as mpl
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['ps.fonttype'] = 42
+
+SAVE_PATH = '../../../data for figures/New/'
 
 
 def gaussian_func(sig, x):
@@ -25,11 +32,11 @@ def mirror_edges(arr, wind_size):
     return np.concatenate((left_edge, arr, right_edge), axis=0)
 
 class STIM_MODE(Enum):
-    ALL = 1
-    SIM = 2
-    CUR = 3
+    ALL = 1  # Any case
+    SIM = 2  # Only simultaneous in all
+    CUR = 3  # Only in current
 
-def get_pair_lst(path, mode = STIM_MODE.ALL):
+def get_pair_lst(path, mode=STIM_MODE.ALL):
     mat = io.loadmat(path, appendmat=False, simplify_cells=True)
     try:
         types = mat['stim']['types']  # Only need pulse
@@ -45,8 +52,8 @@ def get_pair_lst(path, mode = STIM_MODE.ALL):
         else:
             raise NotImplementedError
         durs = np.array([b-a for a, b in stm_pairs])
-        mask = (types == 'PULSE') * (amps > 0.059) * (amps < 0.066) * (durs > 900) * (durs < 1100) * indexes
-        stm_pairs = stm_pairs[mask]
+        mask = (types == 'PULSE') * (durs > 900) * (durs < 1100) * indexes * (amps > 0.059)
+        stm_pairs = stm_pairs[mask.astype('bool')]
 
     except KeyError:
         print(f'Skipping {path} for missing stim and/or times fields')
@@ -58,15 +65,21 @@ def get_pair_lst(path, mode = STIM_MODE.ALL):
 
 def calc_hist(spike_train, stims, bins):
     spike_train = spike_train * 20_000 / 1000
-    ret = np.zeros(len(bins) - 1)
-    for stim in stims:
+    wind_size = int(20 * 3.5 * 2 + 1)
+    g_wind = create_window(wind_size, 20, 3.5)
+    ret = np.zeros((len(stims), len(bins) - 1))
+    for i, stim in enumerate(stims):
         ref_time_list = spike_train - stim
         mask = (ref_time_list >= bins[0]) * (ref_time_list < bins[-1])
         ref_time_list = ref_time_list[mask]
-        hist, _ = np.histogram(ref_time_list, bins=bins)
-        ret += hist
+        hist, _ = np.histogram(ref_time_list, bins=bins) 
+        mirr_hist = mirror_edges(hist, wind_size)
+        conv_hist = np.convolve(mirr_hist, g_wind, mode='valid')
+        ret[i] = conv_hist
 
-    return 20_000 * ret / len(stims)
+    ret *= (20_000 / len(stims))  # TODO this normalization is only valid for 1 sample bin
+
+    return ret.mean(axis=0), stats.sem(ret, axis=0)
 
 
 def check_stim_shank(shank, f):
@@ -114,6 +127,7 @@ def main(data_path, cluster_name, temp_state, ax, mode=STIM_MODE.ALL):
 
     return ax
 
+
 if __name__ == "__main__":
     data_path = '../Data/'
     cluster_name = 'es25nov11_13_3_11'
@@ -135,25 +149,34 @@ if __name__ == "__main__":
         pairs_temp.append(p)
     pairs = combine_list(pairs_temp)
 
-    bins = np.linspace(-50 * 20, 100 * 20, 150 * 20 + 1)
+    c = 20  # TODO allow differnet c values, currently hard coded in calc_hist
+
+    bins = np.linspace(-50 * 20, 100 * 20, 150 * c + 1)
 
     spike_train = cluster.timings
 
-    hist = calc_hist(spike_train, [start for start, end in pairs], bins)
+    hist, sem = calc_hist(spike_train, [start for start, end in pairs], bins)
 
-    wind_size = 141
+    """wind_size = int(c * 3.5 * 2 + 1)
     mirr_hist = mirror_edges(hist, wind_size)
+    mirr_sems = mirror_edges(sems, wind_size)
 
-    g_wind = create_window(wind_size, 20, 3.5)
+    g_wind = create_window(wind_size, c, 3.5)
     conv_hist = np.convolve(mirr_hist, g_wind, mode='valid')
+    conv_sem = np.convolve(mirr_sems, g_wind, mode='valid')"""
 
     fig, ax = plt.subplots(figsize=(30, 8))
-    rect = patches.Rectangle((0, 0), 50, conv_hist.max(), facecolor='c', alpha=0.2)
+    #rect = patches.Rectangle((0, 0), 50, hist.max(), facecolor='b', alpha=0.2)
+    ax.axvline(x=0, ymax=np.max(hist + sem), color='k', linestyle='--')
+    ax.axvline(x=50, color='k', linestyle='--')
+
 
     # Add the patch to the Axes
-    ax.add_patch(rect)
-    ax.plot(np.linspace(-50, 100, 150 * 20), conv_hist)
-    ax.set_ylabel('Hz')
+    #ax.add_patch(rect)
+    ax.plot(np.linspace(-50, 100, 150 * c), hist, color=PV_COLOR)
+    ax.fill_between(np.linspace(-50, 100, 150 * c), hist - sem, hist + sem, color=LIGHT_PV, alpha=0.2)
+    ax.set_ylabel('avg spike/second')
     ax.set_xlabel('ms from onset')
-    ax.axis('off')
-    plt.show()
+
+    #plt.show()
+    plt.savefig(SAVE_PATH + f"PSTH.pdf", transparent=True)

utils/cross_correlation_data.py
@@ -4,6 +4,9 @@ import scipy.io as io
 from preprocessing_pipeline import load_cluster
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 
+SAVE_PATH = '../../../data for figures/New/'
+
+
 def calc_hist(spike_train, stims, bins):
     ret = np.zeros(len(bins) - 1)
     for stim in stims:
@@ -44,6 +47,7 @@ def calc_cc(pv_name, pyr_name, temp_path, ax, loc):
 
     return ax
 
+
 if __name__ == "__main__":
     temp_path = '../temp_state/'
     pyr_name = name = 'es25nov11_13_3_3'  # pyr
@@ -73,5 +77,6 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(figsize=(30, 8))
     ax.vlines(0, ymin=0, ymax=hist.max(), color='k', linestyle='--')
     ax.bar(np.linspace(-50, 50, N - 1), hist)
-    ax.axis('off')
-    plt.show()
+
+    plt.savefig(SAVE_PATH + f"CCH.pdf", transparent=True)
+